d3a539440cbb56fdae8a03c063016f0a93488a0b
Joan Currie <jecurrie8@gmail.com>
2020-01-06 18:12:19 -0500

    Update is_valid_walk.rb

--- is_valid_walk.rb
+++ is_valid_walk.rb
@@ -3,8 +3,3 @@ def is_valid_walk(walk)
   walk.count('n') == walk.count('s') and
   walk.count == 10
 end
-
-##genWalk 10
-# -> ["n", "w", "n", "e", "w", "s", "e", "w", "e", "s"]end
-
-puts is_valid_walk(['n','s','n','s','n','s','n','s','n','s'])
